21a01445b97989717b337593d3c19258b0384c1f
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:13:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -37,16 +37,13 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     language.each do |language, type|
-      binding.pry
       if new_hash.keys.include?(language)
         new_hash[language][:style] << style
       else
         new_hash[language]= type
         new_hash[language][:style] = [style]
       end
-      binding.pry
     end
   end
 end
 
-reformat_languages(languages)
