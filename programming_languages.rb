26dfba1a12e1a160d2c6bc36e2bcd168dabff5f2
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:02:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    binding.pry
+    language.each do |language, type|
+      binding.pry
+      new_hash[language]= language
 end
