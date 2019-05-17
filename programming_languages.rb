15b5b71f2498676579f864cab5737935c9094c8c
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:02:40 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,38 @@
 require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
@@ -6,4 +40,8 @@ def reformat_languages(languages)
     language.each do |language, type|
       binding.pry
       new_hash[language]= language
+    end
+  end
 end
+
+reformat_languages(languages)
