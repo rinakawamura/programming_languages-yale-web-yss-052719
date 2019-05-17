4bb57b3df0958c218a7d66fe18ff9291909d2e32
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 15:14:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
+require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    language.each do |language, type|
+      if new_hash.keys.include?(language)
+        new_hash[language][:style] << style
+      else
+        new_hash[language]= type
+        new_hash[language][:style] = [style]
+      end
+    end
+  end
+  return new_hash
 end
